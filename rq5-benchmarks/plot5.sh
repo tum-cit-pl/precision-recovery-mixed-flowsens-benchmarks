9c480ff8d86b4ae79e04c71749407cbd0f55211c
Michael Schwarz <michael.schwarz93@gmail.com>
2025-03-10 11:11:22 +0100

    Split out plot 5

--- rq5-benchmarks/plot5.sh
+++ rq5-benchmarks/plot5.sh
@@ -6,5 +6,6 @@
 ../helper-scripts/postprocess-csv.py plots/5/plot5-short.csv
 # Move plots where a run failed to a separate file
 ./split-out-fails.py plots/5/plot5-long.csv > plots/5/plot5-long-nofails.csv 2> plots/5/plot5long-fails.csv
+./separate-extra-long.py plots/5/plot5-long-nofails.csv > plots/5/plot5-middle.csv 2> plots/5/plot5-extra.csv
 # Generate the plot
 cd plots/5 && pdflatex plot5.tex && cd ..
