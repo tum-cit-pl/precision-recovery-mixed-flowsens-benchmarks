9044cf0e07899a0db707995d834f5371a454978c
Michael Schwarz <michael.schwarz93@gmail.com>
2025-03-18 17:07:10 +0100

    Fix

--- rq1-3-benchmarks/plot3.sh
+++ rq1-3-benchmarks/plot3.sh
@@ -15,6 +15,6 @@ else
     ./prec_to_csv_cumulative.py --mode cumulative-diff-rec --zero-ambiguous svcomp-raw.txt ours20 base > plots/3/plot3-ours20-vs-base-unambiguous.csv
     ./prec_to_csv_cumulative.py --mode cumulative-diff-rec --zero-ambiguous svcomp-raw.txt ours3 base > plots/3/plot3-ours3-vs-base-unambiguous.csv
     ./prec_to_csv_cumulative.py --mode cumulative-diff-rec --zero-ambiguous svcomp-raw.txt apinis base > plots/3/plot3-apinis-vs-base-unambiguous.csv
-    ./runtimes_to_csv.py --mode count-rec paper-runs/svcomp-raw.txt > plots/3/timeout-counts.tex
+    ./runtimes_to_csv.py --mode count-rec svcomp-raw.txt > plots/3/timeout-counts.tex
 fi
 cd plots/3 && pdflatex plot3.tex && cd ..
