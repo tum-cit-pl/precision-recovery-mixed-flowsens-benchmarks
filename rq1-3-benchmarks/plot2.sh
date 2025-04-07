2f6923b47cbe62449fc909f52ba2111b2a7eb660
Michael Schwarz <michael.schwarz93@gmail.com>
2025-04-07 11:17:56 +0200

    Fix mistake in `plot2.sh` where always paper version was used

--- rq1-3-benchmarks/plot2.sh
+++ rq1-3-benchmarks/plot2.sh
@@ -15,6 +15,6 @@ else
     ./prec_to_csv_cumulative.py --zero-ambiguous svcomp-raw.txt ours20 base > plots/2/plot2-ours20-vs-base-unambiguous.csv
     ./prec_to_csv_cumulative.py --zero-ambiguous svcomp-raw.txt ours3 base > plots/2/plot2-ours3-vs-base-unambiguous.csv
     ./prec_to_csv_cumulative.py --zero-ambiguous svcomp-raw.txt apinis base > plots/2/plot2-apinis-vs-base-unambiguous.csv
-    ./runtimes_to_csv.py --mode count paper-runs/svcomp-raw.txt > plots/2/timeout-counts.tex
+    ./runtimes_to_csv.py --mode count svcomp-raw.txt > plots/2/timeout-counts.tex
 fi
 cd plots/2 && pdflatex plot2.tex && cd ..
